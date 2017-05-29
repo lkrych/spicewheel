ed8386c5fe51ddffc519182279a2cba6ddd7f1a6
Leland Krych <Leland.Krych@gmail.com>
2017-05-29 12:15:35 -0700

    Write destructuring function

--- lib/example.jsx
+++ lib/example.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Spicewheel } from 'spicewheel';
 
 class GenericGrid extends React.Component {
   constructor(props){
@@ -9,23 +8,37 @@ class GenericGrid extends React.Component {
   render(){
     const SpiceRack =
     <table>
-      <tr>
-        <th>Image</th>
-        <th>Label</th>
-      </tr>
-      <tr>
-        <td>"giraffe.jpg"</td>
-        <td>{"a majestic giraffe"}</td>
-      </tr>
-      <tr>
-        <td>"hyena.jpg"</td>
-        <td>{"a mischevious hyena"}</td>
-      </tr>
-      <tr>
-        <td>"elephant.jpg"</td>
-        <td>{"an elegant elephant"}</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Label</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>"giraffe.jpg"</td>
+          <td>{"a majestic giraffe"}</td>
+        </tr>
+        <tr>
+          <td>"hyena.jpg"</td>
+          <td>{"a mischevious hyena"}</td>
+        </tr>
+        <tr>
+          <td>"elephant.jpg"</td>
+          <td>{"an elegant elephant"}</td>
+        </tr>
+      </tbody>
     </table>;
+    const headers = SpiceRack.props.children[0].props.children.props.children.map(tr => tr.props.children);
+    const rows = SpiceRack.props.children[1].props.children.map(tr => tr.props.children);
+    const rowInfo = rows.map(row => row.map(td => td.props.children));
+    const SpiceRackObject = rowInfo.map(row => {
+      return {
+        [headers[0]]: row[0],
+        [headers[1]]: row[1]
+      };
+    });
+    debugger;
     return(
       SpiceRack
     );
