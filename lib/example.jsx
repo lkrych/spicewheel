c4a8bf4739e0bc378b82de3ca4e56f8b5c73bf7f
Leland Krych <Leland.Krych@gmail.com>
2017-05-30 16:41:02 -0700

    Add react form for demo, and start creating util function to fetch image urls

--- lib/example.jsx
+++ lib/example.jsx
@@ -5,6 +5,24 @@ import {spiceWheel} from './components/spicewheel';
 class GenericGrid extends React.Component {
   constructor(props){
     super(props);
+    this.state = {imageNumber: 6,
+                  modern: "checked",
+                  classic: "",
+                  imagesPerRow: 3};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(){}
+
+  handleSelectInput(e){
+    e.prevent_default();
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   render(){
@@ -46,12 +64,63 @@ class GenericGrid extends React.Component {
 
     const spicy = spiceWheel(SpiceRack, "modern", 3);
     return(
-      <div>
+      <div className="demo">
         <h2>SpiceWheel Demo</h2>
-        <p>Welcome to the SpiceWheel demo, choose the number of images you
-           want in your grid, your style and the number of images you want
-            in your row.</p>
-          {SpiceRack}
+        <p>
+          Welcome to the SpiceWheel demo, choose the number of images you
+          want in your grid, your style and the number of images you want
+          in your row.
+        </p>
+
+        <form onSubmit={this.handleSubmit} className="edit-spicerack-form">
+
+          <p>Number of images</p>
+          <select className="form-control"
+            value={this.state.imageNumber}
+            name="imageNumber"
+            onChange={this.handleNumImages}>
+            <option value={6}>6</option>
+            <option value={12}>12</option>
+            <option value={18}>18</option>
+            <option value={24}>24</option>
+          </select>
+
+          <p>Style</p>
+          <div className="checkbox">
+            <label>
+              <input
+                type="checkbox"
+                checked={this.state.modern}
+                />
+                Modern
+            </label>
+          </div>
+          <div className="checkbox ">
+            <label>
+              <input
+                type="checkbox"
+                checked={this.state.classic}
+                />
+                Classic
+            </label>
+          </div>
+
+          <p>Number of images</p>
+          <select className="form-control"
+            value={this.state.imagesPerRow}
+            name="imagesPerRow"
+            onChange={this.handleImagesPerRow}>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select>
+
+
+          <button className="btn btn-primary">Spice up this Safari</button>
+          <span></span>
+        </form>
+        {SpiceRack}
       </div>
     );
   }
